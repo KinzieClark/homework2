f81f468aadad7b1a7341546e9d29297adaa902af
Unknown <kinzie.colleen@gmail.com>
2017-03-30 15:49:55 -0600

    Added Void Functions

---
+++ Homework/Assets/Void Functions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidFunctions : MonoBehaviour {
+
+	// A void function doesn't return any sort of value-- it's mainly used for print commands.
+	void Start () {
+		print("Hello");
+	}
+	
+}
