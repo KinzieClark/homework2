1627f25b1f73d9d662e9abdc6d063fa970479521
Unknown <kinzie.colleen@gmail.com>
2017-03-30 16:04:02 -0600

    More void function code

---
+++ Homework/Assets/VoidFunctions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidFunctions : MonoBehaviour {
+
+	int MyInt = 5;
+
+	// void functions return no value, so they're used for things like print commands
+//	void Start () {
+//		print("Hello");
+//	}
+	
+//	void Start () {
+//		print("How are you?");
+//	}
+
+//	void Start () {
+//		print("How is the weather?");
+//	}
+
+	void Start () {
+		print("Here's a fun fact about void functions:");
+	}
+
+//	void Start () {
+//		print("A void returns no value");
+//	}
+
+//	void Start () {
+//		print(MyInt * 2);
+//	}
+
+//	void Start () {
+//		print("It'll do basic things, like an int * 2");
+//	}
+
+//	void Start () {
+//		print("If you want to get more detailed, try a different function");
+//	}
+}
